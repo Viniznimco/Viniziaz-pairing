1aedd54ebe36bf95131bc6e99e870ba8da7b7cf1
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-22 17:20:56 +0300

    Pair

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'kkeizzah@gmail.com', // Replace with your Mega email
-    password: 'Keith4189baraza@', // Replace with your Mega password
+    email: 'msnapdragon@gmail.com', // Replace with your Mega email
+    password: 'snapdragon142@', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
